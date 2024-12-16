53670331e7a6671d15e103ce23788eebb552402d
VladislavGolovachev <serpentess14@gmail.com>
2024-12-16 15:38:08 +0500

    Initial commit

---
+++ MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by Владислав Головачев on 16.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
